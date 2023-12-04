c3e32c9486891d1e07737604c1a0170e29c4f3a3
SAM MESHACH A <sammeshach333@gmail.com>
2023-12-04 10:15:06 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+    {
+        name: "Naan Ready than",
+        artist: "Anirudh",
+        img: "music-1",
+        src: "music-1",
+    },
+    {
+        name: "Anbennum",
+        artist: "Anirudh",
+        img: "music-2",
+        src: "music-2",
+    },
+    {
+        name: "Rolex Bgm",
+        artist: "Sam meshach",
+        img: "music-3",
+        src: "music-3",
+    },
+    {
+        name: "Edhathan Kandutte",
+        artist: "Gana Bala",
+        img: "music-4",
+        src: "music-4",
+    },
+    {
+        name: "Toxicity",
+        artist: "System Of A Down",
+        img: "music-5",
+        src: "music-5",
+    },
+    {
+        name: "Master of Puppets (Remastered)",
+        artist: "Metallica",
+        img: "music-6",
+        src: "music-6",
+    },
+    {
+        name: "Enter Sandman",
+        artist: "Metallica",
+        img: "music-7",
+        src: "music-7",
+    },
+    {
+        name: "Dream On",
+        artist: "Aerosmith",
+        img: "music-8",
+        src: "music-8",
+    },
+];
